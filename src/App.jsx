5dc2507ed9d89628d702c1487df9e0ad2f404c73
박광민 <rhkdals0242@naver.com>
2025-04-16 19:35:17 +0900

    ♻️ refactor: App 컴포넌트 export 방식 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
-function App() {
+import { Value } from 'sass';
+import './assets/styles/variables.scss';
+import Button from './components/common/Button';
+
+export default function App() {
   return (
     <div className="app">
       <h1>롤링 페이퍼 프로젝트</h1>
     </div>
   );
 }
-
-export default App;
