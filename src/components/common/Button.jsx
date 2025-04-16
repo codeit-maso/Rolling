08206c5dff05771513607978de6689e7f6810929
박광민 <rhkdals0242@naver.com>
2025-04-16 19:21:16 +0900

    ✨ feat: 공통 버튼 기능 구현

---
+++ src/components/common/Button.jsx
@@ -0,0 +1,13 @@
+import styles from './Button.module.scss';
+
+export default function Button({ text, type = 'primary', onClick, disabled }) {
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      className={`${styles.button} ${styles[type]}`}
+    >
+      {text}
+    </button>
+  );
+}
