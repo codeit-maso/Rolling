ef2d4bb1dc346e361d824c60f23a075e1bf7c059
Yun-Jinwoo <yjw3480@naver.com>
2025-04-18 00:03:14 +0900

    🐛 fix: 컨벤션에 맞게 코드 포매팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import "./base.scss";
-import { Route, Routes } from "react-router-dom";
-import Header from "./components/layout/Header/Header";
+import '../src/assets/styles/base.scss';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/layout/Header/Header';
 import Test from './pages/List-page/Test';
 
 export default function App() {
   return (
     <>
-      <Header/>
+      <Header />
       <Routes>
-        <Route path="/" element={<Test/>}/>
+        <Route path="/" element={<Test />} />
       </Routes>
     </>
   );
