88c8c9ba5d6a669dff95adbc8e513adf954ad06e
jyn <spdlqjdkslel1226@naver.com>
2025-04-17 14:50:56 +0900

    📁 chore: push 하기 전에 conflict 발생할 파일 제거

--- src/pages/List-page/listApi.jsx
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-export async function getList(){
-    const response = await axios.get("https://rolling-api.vercel.app/13-2/recipients/");
-    const {data} = response;
-    return data;
-}
