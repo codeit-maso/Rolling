9a3de4a67435d0e4e3b24c305febcf5e87b0ab86
박광민 <rhkdals0242@naver.com>
2025-04-18 00:51:33 +0900

    📁 chore: eslint 및 prettier 적용

--- src/pages/List-page/Test.jsx
+++ src/pages/List-page/Test.jsx
@@ -1,5 +1,3 @@
-export default function Test(){
-    return(
-        <div style={{fontSize:"30px"}}>Route test component ^_^ </div>
-    )
+export default function Test() {
+  return <div style={{ fontSize: '30px' }}>Route test component ^_^ </div>;
 }
