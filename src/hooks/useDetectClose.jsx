47b7b40a4d130ab66f2596afe44d4aa39b7c8d53
박광민 <rhkdals0242@naver.com>
2025-04-20 06:50:59 +0900

    ✨ feat: 드랍다운 외부 클릭시 닫히는 기능 구현

---
+++ src/hooks/useDetectClose.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react';
+
+export default function useDetectClose(ref) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  useEffect(() => {
+    const handleClick = (e) => {
+      if (ref.current && !ref.current.contains(e.target)) {
+        setIsOpen(!isOpen);
+      }
+    };
+    if (isOpen) {
+      document.addEventListener('click', handleClick);
+    }
+    return () => {
+      document.removeEventListener('click', handleClick);
+    };
+  }, [isOpen, ref]);
+  return [isOpen, setIsOpen];
+}
