9f8a093bd428f05701107e599a9da43f370f1626
Yun-Jinwoo <yjw3480@naver.com>
2025-04-19 17:34:43 +0900

    🐛 fix: Card, Badge 폴더 위치 이동으로 인한 경로 수정

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -1,3 +1,37 @@
+// Posts.jsx
+import { useParams } from 'react-router';
+import { useEffect, useState } from 'react';
+import getRecipient from '../../api/getRecipient';
+import Card from '../../components/Card/Card';
+import styles from './Posts.module.scss'; // SCSS 파일 import
+
 export default function Posts() {
-  return <div style={{ fontSize: '30px' }}>Route test component ^_^ </div>;
+  const { id } = useParams();
+  const [postData, setPostData] = useState(null);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        const result = await getRecipient(id);
+        setPostData(result);
+      } catch (error) {
+        console.error('데이터 로딩 실패:', error.response.data);
+      }
+    };
+
+    fetch();
+  }, [id]);
+
+  if (!postData) return <div>Loading...</div>;
+
+  return (
+    <div
+      className={`${styles['post-container']} ${!postData.backgroundImageURL ? styles[`background--${postData.backgroundColor}`] : ''}`}
+      style={
+        postData.backgroundImageURL
+          ? { backgroundImage: `url(${postData.backgroundImageURL})` }
+          : {}
+      }
+    ></div>
+  );
 }
