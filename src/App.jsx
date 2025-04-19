10a0ce004207a5305f55bb4712e285f828a068c6
Yun-Jinwoo <yjw3480@naver.com>
2025-04-19 17:37:16 +0900

    📁 chore: 컴포넌트명 변경 ( posts -> recipient )

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Header from './components/layout/Header/Header';
 import Home from './pages/Home/Home';
 import RecipientList from './pages/RecipientList/RecipientList';
 import CreateRecipient from './pages/CreateRecipient/CreateRecipient';
-import Posts from './pages/Posts/Posts';
+import Recipient from './pages/Recipient/Recipient';
 import MessageForm from './pages/MessageForm/MessageForm';
 
 export default function App() {
@@ -15,7 +15,7 @@ export default function App() {
         <Route path="/" element={<Home />} />
         <Route path="/list" element={<RecipientList />} />
         <Route path="/post" element={<CreateRecipient />} />
-        <Route path="/post/:id" element={<Posts />} />
+        <Route path="/post/:id" element={<Recipient />} />
         <Route path="/post/:id/message" element={<MessageForm />} />
       </Routes>
     </>
