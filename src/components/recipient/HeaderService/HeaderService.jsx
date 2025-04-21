1eae862c6a41d27125fe62ae7d44c61d9b6102a4
LeeCh0129 <cksgh5477@gmail.com>
2025-04-22 02:30:03 +0900

    ✨ feat: HeaderService 프로필 이미지 섹션 및 아이콘 추가

--- src/components/recipient/HeaderService/HeaderService.jsx
+++ src/components/recipient/HeaderService/HeaderService.jsx
@@ -1,5 +1,7 @@
 import styles from './HeaderService.module.scss';
-
+import emojiAdd from '../../../assets/images/emoji-add.svg';
+import shareIcon from '../../../assets/images/share.svg';
+import chevronDown from '../../../assets/images/chevron-down.svg';
 export default function HeaderService({ recipient }) {
   return (
     <div className={styles['header-service']}>
@@ -11,18 +13,40 @@ export default function HeaderService({ recipient }) {
         </div>
 
         <div className={styles['header-service__profile-section']}>
+          <div className={styles['header-service__profile-images']}>
+            {recipient?.recentMessages?.slice(0, 3).map((message, index) => (
+              <img
+                key={message.id}
+                src={message.profileImageURL}
+                alt="프로필"
+                className={styles['header-service__profile-image']}
+                style={{ zIndex: index + 1 }}
+              />
+            ))}
+            {recipient?.messageCount > 3 && (
+              <div className={styles['header-service__additional-count']}>
+                +{recipient.messageCount - 3}
+              </div>
+            )}
+          </div>
           <span className={styles['header-service__message-count']}>
             {recipient?.messageCount || 0}명이 작성했어요!
           </span>
         </div>
 
-        <div className={styles['header-service__divider']}></div>
-
+        <div className={styles['header-service__divider--left']}></div>
         <div className={styles['header-service__actions']}>
           <div className={styles['header-service__emojis']}>이모지</div>
-          <button className={styles['header-service__add-button']}>추가</button>
+          <button className={styles['header-service__emoji-more']}>
+            <img src={chevronDown} alt="이모지 더보기" />
+          </button>
+          <button className={styles['header-service__add-button']}>
+            <img src={emojiAdd} alt="이모지 추가" />
+            &nbsp;추가
+          </button>
+          <div className={styles['header-service__divider--right']}></div>
           <button className={styles['header-service__share-button']}>
-            공유
+            <img src={shareIcon} alt="공유" />
           </button>
         </div>
       </div>
--- src/pages/Recipient/Recipient.jsx
+++ src/pages/Recipient/Recipient.jsx
@@ -1,9 +1,30 @@
 import HeaderService from '../../components/recipient/HeaderService/HeaderService';
+import checkedSvg from '../../assets/images/checked.svg';
 
 export default function Recipient() {
+  // 임시 목데이터 : 프로필 UI 확인용 기능 구현 후 삭제예정
+  const mockRecipient = {
+    name: 'Chan Ho',
+    messageCount: 6,
+    recentMessages: [
+      {
+        id: 1,
+        profileImageURL: checkedSvg,
+      },
+      {
+        id: 2,
+        profileImageURL: checkedSvg,
+      },
+      {
+        id: 3,
+        profileImageURL: checkedSvg,
+      },
+    ],
+  };
+
   return (
     <div>
-      <HeaderService />
+      <HeaderService recipient={mockRecipient} />
     </div>
   );
 }
