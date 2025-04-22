61c7f8174a4c7e989e724d7cceeea102ab9ba0b6
Yun-Jinwoo <yjw3480@naver.com>
2025-04-22 13:39:18 +0900

    ✨ feat: Recipient 페이지 무한스크롤 구현

--- src/pages/Recipient/Recipient.jsx
+++ src/pages/Recipient/Recipient.jsx
@@ -1,5 +1,5 @@
 import { useParams } from 'react-router';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import getRecipient from '../../api/getRecipient';
 import getMessages from '../../api/getMessages';
 import Card from '../../components/Card/Card';
@@ -10,11 +10,17 @@ export default function Recipient() {
   const [postData, setPostData] = useState(null);
   const [messages, setMessages] = useState([]);
   const [offset, setOffset] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [hasNextMessage, setHasNextMessage] = useState(false);
+  const observerRef = useRef();
+
   useEffect(() => {
     const fetchRecipient = async () => {
       try {
         const recipient = await getRecipient(id);
         setPostData(recipient);
+        setHasNextMessage(recipient.messageCount > 0);
+        setLoading(false);
       } catch (error) {
         console.error('데이터 로딩 실패:', error.response.data);
       }
@@ -24,29 +30,47 @@ export default function Recipient() {
   }, [id]);
 
   useEffect(() => {
+    setLoading(true);
     const fetchMessages = async () => {
       try {
         const limit = offset === 0 ? 8 : 9;
         const newMessages = await getMessages(id, offset, limit);
-        if (offset === 0) {
-          setMessages(newMessages.results);
-        } else {
-          setMessages((prev) => [...prev, ...newMessages.results]);
-        }
+        offset === 0
+          ? setMessages(newMessages.results)
+          : setMessages((prev) => [...prev, ...newMessages.results]);
+        if (!postData) return;
+        setHasNextMessage(offset < postData.messageCount);
+        setLoading(false);
       } catch (error) {
-        console.error('데이터 로딩 실패:', error.response.data);
+        console.error(
+          '데이터 로딩 실패:',
+          error.response?.data || error.message,
+        );
       }
     };
 
     fetchMessages();
   }, [id, offset]);
 
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      const firstEntry = entries[0];
+      if (firstEntry.isIntersecting && hasNextMessage && !loading) {
+        loadMoreMessages();
+      }
+    });
+    if (observerRef.current) observer.observe(observerRef.current);
+    return () => {
+      if (observerRef.current) observer.unobserve(observerRef.current);
+    };
+  }, [hasNextMessage, loading]);
+
   const loadMoreMessages = () => {
     const limit = offset === 0 ? 8 : 9;
     setOffset((prev) => prev + limit);
   };
 
-  if (!postData) return <div>Loading...</div>;
+  if (!postData || messages.length === 0) return <div>Loading...</div>;
 
   return (
     <div
@@ -71,7 +95,7 @@ export default function Recipient() {
           </Card>
         ))}
       </div>
-      <button onClick={loadMoreMessages}>더보기</button>
+      {hasNextMessage && <div ref={observerRef} className="load"></div>}
     </div>
   );
 }
