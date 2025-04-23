d98ebb2e13098f49ac22b73aaffe9319b54b1e29
haru <ju30359@gmail.com>
2025-04-23 12:08:00 +0900

    🎨 : css파일 -> module.scss로 변경, 반응형 구 

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import './assets/styles/base.scss';
 import { Route, Routes } from 'react-router-dom';
 import Header from './components/layout/Header/Header';
 import Test from './pages/List-page/Test';
-import Home from './pages/Home/home';
+import Home from './pages/Home/Home';
 
 export default function App() {
   return (
