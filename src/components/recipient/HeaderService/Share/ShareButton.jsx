d1c08c71133a81e43c3094ade0e713ea8335ff21
LeeCh0129 <cksgh5477@gmail.com>
2025-04-24 16:40:53 +0900

    ✨ feat: 카카오톡 공유 기능 구현

--- src/components/recipient/HeaderService/Share/ShareButton.jsx
+++ src/components/recipient/HeaderService/Share/ShareButton.jsx
@@ -1,10 +1,17 @@
 import { useState, useRef, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from './ShareButton.module.scss';
 import shareIcon from '../../../../assets/images/share.svg';
+import { initializeKakaoSDK, shareKakao } from '../../../../utils/share';
 
 export default function ShareButton({ recipient }) {
   const [showShareOptions, setShowShareOptions] = useState(false);
   const shareButtonRef = useRef(null);
+  const location = useLocation();
+
+  useEffect(() => {
+    initializeKakaoSDK();
+  }, []);
 
   useEffect(() => {
     const handleClickOutside = (event) => {
@@ -26,13 +33,26 @@ export default function ShareButton({ recipient }) {
     setShowShareOptions(!showShareOptions);
   };
 
+  const handleKakaoShare = () => {
+    const currentUrl = window.location.origin + location.pathname;
+    shareKakao(recipient, currentUrl);
+    setShowShareOptions(false);
+  };
+
   return (
     <div className={styles['share-button-container']} ref={shareButtonRef}>
       <button className={styles['share-button']} onClick={toggleShareOptions}>
         <img src={shareIcon} alt="공유" />
       </button>
 
-      {/* TODO: 공유 옵션 드롭다운은 추후 구현 */}
+      {showShareOptions && (
+        <div className={styles['share-options']}>
+          <button className={styles['share-option']} onClick={handleKakaoShare}>
+            카카오톡 공유
+          </button>
+          <button className={styles['share-option']}>URL 공유</button>
+        </div>
+      )}
     </div>
   );
 }
