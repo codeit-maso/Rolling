c462f690e30e45e560e27c6520ed030840ca6b48
haru <ju30359@gmail.com>
2025-04-25 18:08:55 +0900

    🎨style : ui수정 및 반응형 구현 완료

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,5 @@
 import styles from './Home.module.scss';
-import img01 from '../../assets/images/img_01.png';
+import img01 from '../../assets/images/img_01.svg';
 import img02 from '../../assets/images/img_02.png';
 import Button from '../../components/common/Button';
 import { useNavigate } from 'react-router-dom';
@@ -9,41 +9,45 @@ export default function Home() {
 
   return (
     <div className={styles.homeWrapper}>
-      <section className={`${styles.sectionBox} ${styles.rightImage}`}>
-        <img
-          src={img01}
-          alt="롤링페이퍼 이미지"
-          className={styles.sectionImage}
-        />
-        <div className={styles.textBox}>
-          <div className={styles.pointBadge}>Point.01</div>
-          <h2 className={styles.title}>
-            누구나 손쉽게, 온라인
-            <br />
-            롤링 페이퍼를 만들 수 있어요
-          </h2>
-          <p className={styles.subtext}>로그인 없이 자유롭게 만들어요.</p>
-        </div>
+      <section className={styles.sectionBoxes}>
+        <article className={`${styles.sectionBox} ${styles.rightImage}`}>
+          <div className={styles.textBox}>
+            <div className={styles.pointBadge}>Point.01</div>
+            <h2 className={styles.title}>
+              <span>누구나 손쉽게, 온라인</span> 롤링 페이퍼를 만들 수 있어요
+            </h2>
+            <p className={styles.subtext}>로그인 없이 자유롭게 만들어요.</p>
+          </div>
+          <img
+            src={img01}
+            alt="롤링페이퍼 이미지"
+            className={styles.sectionImage}
+          />
+        </article>
       </section>
 
-      <section className={`${styles.sectionBox} ${styles.leftImage}`}>
-        <img src={img02} alt="이모지 이미지" className={styles.sectionImage} />
-        <div className={styles.textBox}>
-          <div className={styles.pointBadge}>Point.02</div>
-          <h2 className={styles.title}>
-            서로에게 이모지로 감정을
-            <br />
-            표현해주세요
-          </h2>
-          <p className={styles.subtext}>
-            롤링 페이퍼에 이모지를 추가할 수 있어요.
-          </p>
-        </div>
+      <section className={styles.sectionBoxes}>
+        <article className={`${styles.sectionBox} ${styles.leftImage}`}>
+          <div className={styles.textBox}>
+            <div className={styles.pointBadge}>Point.02</div>
+            <h2 className={styles.title}>
+              <span>서로에게 이모지로 감정을</span> 표현해보세요
+            </h2>
+            <p className={styles.subtext}>
+              롤링 페이퍼에 이모지를 추가할 수 있어요.
+            </p>
+          </div>
+          <img
+            src={img02}
+            alt="이모지 이미지"
+            className={styles.sectionImage}
+          />
+        </article>
       </section>
 
-      <div className={styles.buttonWrapper}>
-        <Button onClick={() => navigate('/list')}>구경해보기</Button>
-      </div>
+      <Button className="buttonWrapper" onClick={() => navigate('/list')}>
+        구경해보기
+      </Button>
     </div>
   );
 }
