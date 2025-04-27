f405baadbfcdde343c01b397fd0571ca01ef402f
박광민 <rhkdals0242@naver.com>
2025-04-27 22:00:31 +0900

    ✨ feat: 프로필 이미지 업로드 기능 추가 및 UI 개선

--- src/components/UserProfileSelector/UserProfileSelector.jsx
+++ src/components/UserProfileSelector/UserProfileSelector.jsx
@@ -1,5 +1,6 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import getProfileImages from '../../api/getProfileImages.js';
+import uploadImage from '../../api/postUpLoadImage.js';
 import DEFAULT_PROFILE_IMAGE from '../../constants/image.js';
 import styles from './UserProfileSelector.module.scss';
 
@@ -9,6 +10,7 @@ export default function UserProfileSelector({
 }) {
   const [profileImages, setProfileImages] = useState([]);
   const [loadedImages, setLoadedImages] = useState({});
+  const fileInput = useRef(null);
 
   useEffect(() => {
     const loadImages = async () => {
@@ -22,18 +24,42 @@ export default function UserProfileSelector({
     setLoadedImages((prev) => ({ ...prev, [url]: true }));
   };
 
+  const handleUpload = async (e) => {
+    const file = e.target.files[0];
+    if (!file) return;
+
+    try {
+      const uploadedUrl = await uploadImage(file);
+      onSelect?.(uploadedUrl);
+    } catch (error) {
+      alert('이미지 업로드 실패했습니다.');
+    }
+  };
+
+  const triggerFileSeletor = () => {
+    fileInput.current?.click();
+  };
+
   return (
     <div className={styles['profile-select']}>
       <h2 className={styles['profile-select__title']}>프로필 이미지</h2>
       <div className={styles['profile-select__content']}>
         <img
           src={value}
-          alt="선택된 프로필"
+          alt="선택된 프로필 및 업로드 이미지"
           className={styles['profile-select__selected-image']}
+          onClick={triggerFileSeletor}
+        />
+        <input
+          type="file"
+          accept="image/"
+          onChange={handleUpload}
+          ref={fileInput}
+          style={{ display: 'none' }}
         />
         <div className={styles['profile-select__right']}>
           <p className={styles['profile-select__description']}>
-            프로필 이미지를 선택해주세요!
+            프로필을 클릭해 업로드해 주세요!
           </p>
           <div className={styles['profile-select__image-list']}>
             {profileImages.map((url, idx) => (
